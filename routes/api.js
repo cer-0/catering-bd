b2b88be1701fd848c6a478cccad21494a4950907
cer-0 <cer0@riseup.net>
2024-06-26 22:55:12 -0600

    Agrega endopoint clientes, sucursales y empleados
    Se incluyen tres endpoints más de modo lectura (R) para consultar los
    siguientes datos de la BD:
    - sucursales: devuelve una lista de las sucursales.
    - clientes: devuelve una lista de los nombres de los clientes.
    - empleados: devuelve una lista con el nombre completo de los empleados.

--- routes/api.js
+++ routes/api.js
@@ -24,4 +24,70 @@ router.get("/cantidad_de_productos", async (req, res) => {
     }
 );
 
+router.get("/sucursales", async (req, res) => {
+    let connection;
+    try {
+        connection = await db.initialize();
+        const result = await connection.execute("select nombre from sucursal");
+        const cantidad = result.rows;
+        res.send(cantidad);
+    } catch (err) {
+        console.error(err);
+        res.sendStatus(500);
+    } finally {
+        if (connection) {
+            try {
+                await connection.close();
+            } catch (err) {
+            console.error(err);
+            }
+        }
+    }
+    }
+);
+
+
+router.get("/empleados", async (req, res) => {
+    let connection;
+    try {
+        connection = await db.initialize();
+        const result = await connection.execute("select nombre || ' ' || apellido_paterno || ' ' || apellido_materno from empleado");
+        const cantidad = result.rows;
+        res.send(cantidad);
+    } catch (err) {
+        console.error(err);
+        res.sendStatus(500);
+    } finally {
+        if (connection) {
+            try {
+                await connection.close();
+            } catch (err) {
+            console.error(err);
+            }
+        }
+    }
+    }
+);
+
+router.get("/clientes", async (req, res) => {
+    let connection;
+    try {
+        connection = await db.initialize();
+        const result = await connection.execute("select nombre from cliente");
+        const cantidad = result.rows;
+        res.send(cantidad);
+    } catch (err) {
+        console.error(err);
+        res.sendStatus(500);
+    } finally {
+        if (connection) {
+            try {
+                await connection.close();
+            } catch (err) {
+            console.error(err);
+            }
+        }
+    }
+    }
+);
 module.exports = router;
